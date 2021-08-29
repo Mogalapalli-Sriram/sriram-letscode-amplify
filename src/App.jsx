f36325c24e1dfe14c225b06a2d5e0b5f3f47a60d
sri ram mogalapalli <srirammogalapalli@sris-MacBook-Air.local>
2021-08-29 23:29:40 +0530

    media player added and some functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import React, {useEffect, useState} from 'react';
-import Amplify , {API , graphqlOperation, input, sectionFooter} from 'aws-amplify';
+import Amplify , {API , graphqlOperation, Storage} from 'aws-amplify';
 import awsconfig from './aws-exports';
 import {AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react';
 import {listSongs} from './graphql/queries';
@@ -9,12 +9,14 @@ import {Paper , IconButton} from '@material-ui/core';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import PauseIcon from '@material-ui/icons/Pause';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import ReactPlayer from 'react-player';
 Amplify.configure(awsconfig);
 
 function App() {
 
   const [songs, setSongs] = useState([]);
   const [songPlaying, setSongPlaying] = useState("");
+  const [audioUrl, setAudioUrl] = useState("");
   useEffect(() => {
     fetchsongs();
     console.log("Hi I am useEffect, which gets executed on loading the web page.")
@@ -25,6 +27,7 @@ function App() {
       const songsList = songsData.data.listSongs.items;
       console.log('songs list',songsList);
       setSongs(songsList);
+
     }catch(err) {
       console.log("Error fetching the songs", err);
     }
@@ -51,8 +54,19 @@ function App() {
         setSongPlaying("");
         return;
       }
-      setSongPlaying(index);
-      return;
+      const songFilePath = songs[index].filePath;
+      try {
+       const fileAccessUrl = await Storage.get(songFilePath, {expires: 60});
+       console.log("access the url", fileAccessUrl);
+       setSongPlaying(index);
+       setAudioUrl(fileAccessUrl);
+       return;
+      } catch(err) {
+        console.log("error in getting the song from the path", err);
+        setSongPlaying("");
+        setAudioUrl("");
+      }
+     
   }
 
   return (
@@ -83,7 +97,18 @@ function App() {
                 </div>
                 <div className="songDescription">{song.description}</div>
                 </div>
-                
+                {
+                  songPlaying === idx ? (
+                    <div>
+                      <ReactPlayer 
+                      url={audioUrl}
+                      controls
+                      height = "50px"
+                      onPause = {() => toggleIcon(idx)}
+                      />
+                    </div>
+                  ) : null
+                }
                 
               </Paper>
             )})
