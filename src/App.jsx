73b7b4bab00fa18c73967253ca9c1c3fffe1f811
sri ram mogalapalli <srirammogalapalli@sris-MacBook-Air.local>
2021-08-29 22:03:32 +0530

    modified the code and added the storage

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import './App.css';
+import React, {useEffect, useState} from 'react';
+import Amplify , {API , graphqlOperation, input, sectionFooter} from 'aws-amplify';
+import awsconfig from './aws-exports';
+import {AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react';
+import {listSongs} from './graphql/queries';
+import {updateSong} from './graphql/mutations';
+import {Paper , IconButton} from '@material-ui/core';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PauseIcon from '@material-ui/icons/Pause';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+Amplify.configure(awsconfig);
+
+function App() {
+
+  const [songs, setSongs] = useState([]);
+  const [songPlaying, setSongPlaying] = useState("");
+  useEffect(() => {
+    fetchsongs();
+    console.log("Hi I am useEffect, which gets executed on loading the web page.")
+  }, [])
+  const fetchsongs = async () => {
+    try {
+      const songsData = await API.graphql(graphqlOperation(listSongs));
+      const songsList = songsData.data.listSongs.items;
+      console.log('songs list',songsList);
+      setSongs(songsList);
+    }catch(err) {
+      console.log("Error fetching the songs", err);
+    }
+  }
+
+  const addLike = async (index) => {
+    try {
+      const song = songs[index];
+      song.likes = song.likes+ 1;
+      delete song.createdAt;
+      delete song.updatedAt;
+      const songData = await API.graphql(graphqlOperation(updateSong, {input: song}));
+      const songList = [...songs];
+      songList[index] = songData.data.updateSong;
+      setSongs(songList);
+    } catch(err) {
+      console.log("error which clicking the song like", err);
+    }
+  }
+
+  const toggleIcon = async (index) => {
+
+      if(songPlaying === index) {
+        setSongPlaying("");
+        return;
+      }
+      setSongPlaying(index);
+      return;
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AmplifySignOut />
+        <h1> Hello World Amplify</h1>
+      </header>
+      <div className="songList">
+          {songs.map((song, idx) => {
+
+            return(
+              <Paper variant="outlined" elevation= {2} key={`song${idx}`} >
+                <div className="songCard">
+                <IconButton aria-label="play" onClick={() => toggleIcon(idx)}>
+                   {songPlaying === idx ? <PauseIcon /> : <PlayArrowIcon /> }
+                </IconButton>
+                <div>
+                <div className="songTitle">{song.name}</div>
+                <div className="songOwner">{song.owner}</div>
+                </div>
+                <div>
+                <IconButton aria-label="like" onClick={() => addLike(idx)}>
+                  <FavoriteIcon />
+                </IconButton>
+                 {song.likes}
+                 
+                </div>
+                <div className="songDescription">{song.description}</div>
+                </div>
+                
+                
+              </Paper>
+            )})
+
+          }
+        </div>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
